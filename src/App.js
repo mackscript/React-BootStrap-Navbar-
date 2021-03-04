654d73569efef93f578d16935e4ae505801da3f5
mack <mackparker001@gmail.com>
2021-03-04 18:49:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import * as ReactBootStrap from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ReactBootStrap.Navbar bg='light' expand='lg'>
+        <ReactBootStrap.Navbar.Brand href='#home'>
+          React-Bootstrap
+        </ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <ReactBootStrap.Navbar.Collapse id='basic-navbar-nav'>
+          <ReactBootStrap.Nav className='mr-auto'>
+            <ReactBootStrap.Nav.Link href='#home'>Home</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href='#link'>Link</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href='#home'>Home</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href='#link'>Link</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href='#home'>Home</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href='#link'>Link</ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+          {/* <ReactBootStrap.Form inline>
+            <ReactBootStrap.FormControl
+              type='text'
+              placeholder='Search'
+              className='mr-sm-2'
+            />
+            
+          </ReactBootStrap.Form> */}
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Navbar>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
